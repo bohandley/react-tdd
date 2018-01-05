6afadec4c54637b0fb87591ecdff69f45b7fcc87
Brendan O'Handley <brendan.ohandley@gmail.com>
2018-01-05 14:56:21 -0600

    add jasmine module, write tests for sayHello.js greet in sayHelloSpec.js, red green refactor, add jasmine:true to .eslintrc in env, add jasmine to test script in package.json, test build

---
+++ spec/sayHelloSpec.js
@@ -0,0 +1,15 @@
+const sayHello = require('../src/sayHello.js');
+
+describe('Greet', function() { 
+    it('concats Hello and a name', function() {
+        let actual = sayHello.greet('World');     
+        let expected = 'Hello, World';
+        expect(actual).toEqual(expected); 
+    });
+
+    it('returns Hello, Friend when no argument is passed', function() {
+        let actual = sayHello.greet();
+        let expected = 'Hello, Friend';
+        expect(actual).toEqual(expected);
+    });
+});
