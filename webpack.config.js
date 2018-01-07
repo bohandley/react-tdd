a60cb13ee73d2f6800dc6adbe5166ba68364ed84
Brendan O'Handley <brendan.ohandley@gmail.com>
2018-01-07 13:12:42 -0600

    modify karma config to use webpack to bundle files for testing, add bundling of assets and creation of the dist directory as a step in the build process.

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,19 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path'); 
 
 module.exports = {
-  devtool: 'source-map',
-  entry  : './src/scripts/app.js',     
-  output : {
-    path: path.resolve(__dirname, 'dist'),
-    filename : './dist/scripts/app.js'
-  },
-  plugins :[
-    new CleanWebpackPlugin('dist'),
-    new HTMLWebpackPlugin({
-      filename: 'index.html',
-      title: 'React-TDD',
-      mainDiv: 'welcome-message',
-      template: 'src/index.html'
-    })
-  ]
+    devtool: 'source-map',
+    entry  : './src/scripts/app.js',     
+    output : {
+        path: path.resolve(__dirname, 'dist'),
+        filename : './dist/scripts/app.js'
+    },
+    plugins :[
+        new CleanWebpackPlugin('dist'),
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            title: 'React-TDD',
+            mainDiv: 'welcome-message',
+            template: 'src/index.html'
+        })
+    ]
 };
