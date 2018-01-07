8610c4e75e405269d1c8ff72c204314761047ac4
Brendan O'Handley <brendan.ohandley@gmail.com>
2018-01-07 15:38:23 -0600

    convert to ES6 syntax using 'import * as something from './something.js'' and arrow function syntax, 'export let nameOfFunction = (par) => {code}

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,8 @@
-console.log('Hello world!');
-var sayHello = require('./sayHello.js');
+// ES6 syntax
+import * as sayHello from './sayHello.js'
 window.addEventListener('load',()=>{
     document.getElementById('welcome-message').innerHTML = sayHello.greet('Brendan');
 });
+
+// console.log('Hello world!');
+// var sayHello = require('./sayHello.js');
--- src/scripts/sayHello.js
+++ src/scripts/sayHello.js
@@ -1,7 +1,24 @@
-exports.greet = function greet(name) {
-    if ( name === undefined ) {
-        return 'Hello, Friend';
-    } else {
-        return 'Hello, ' + name;
-    }
-};
+// ES6 syntax
+export let greet = (name) => {
+  if ( name === undefined ) {
+      return 'Hello, Friend';
+  } else {
+      return 'Hello, ' + name;
+  }  
+}
+
+// export function greet(name {
+//   if ( name === undefined ) {
+//       return 'Hello, Friend';
+//   } else {
+//       return 'Hello, ' + name;
+//   }
+// });
+
+// exports.greet = function greet(name) {
+//     if ( name === undefined ) {
+//         return 'Hello, Friend';
+//     } else {
+//         return 'Hello, ' + name;
+//     }
+// };
